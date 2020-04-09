56391fb6d1840cc3765a7ff1038c0f6b379aced9
G8A4W0416 <wilhelmg1@gmail.com>
2020-04-08 21:52:44 -0500

    Second commit for test_object_created_all_attributes unit test passing successfully.

--- test/test_student.py
+++ test/test_student.py
@@ -14,6 +14,13 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.student.first_name, 'Greg')
         self.assertEqual(self.student.major, 'Java Application Developer Certificate')
 
+    def test_object_created_all_attributes(self):
+        student = t.Student('Wilhelm', 'Greg', 'Java Application Developer Certificate', 4.0)
+        assert student.last_name == 'Wilhelm'
+        assert student.first_name == 'Greg'
+        assert student.major == 'Java Application Developer Certificate'
+        assert student.gpa == 4.0
+
 
 if __name__ == '__main__':
     unittest.main()
