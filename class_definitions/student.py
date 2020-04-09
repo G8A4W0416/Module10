44f7ded15e2eb187fc4b47cb939e9eafb6f2b96b
G8A4W0416 <wilhelmg1@gmail.com>
2020-04-08 23:39:06 -0500

    11th commit for test_object_not_created_error_gpa unit test passing with exception raised if gpa is not a float or falls outside of the gpa range.

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -6,6 +6,13 @@ class Student:
         if not (characters.issuperset(lname) and characters.issuperset(fname) and
                 characters.issuperset(major)):
             raise ValueError
+
+        if gpa and not isinstance(gpa, float):
+            raise ValueError
+
+        if gpa and gpa < 0.0 or gpa > 4.0:
+            raise ValueError
+
         self.last_name = lname
         self.first_name = fname
         self.major = major
